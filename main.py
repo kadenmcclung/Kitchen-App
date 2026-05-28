793618f88d177946e77260b0d45cc7b70d01f172
kadenmcclung <kadenmcclung@gmail.com>
2026-05-28 16:44:45 -0500

    Fix Testing

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ def run_test_mode():
         print("IMPORT FAILURE:", e)
         raise
 
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "test":
         run_test_mode()
